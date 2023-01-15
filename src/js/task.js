36ec157b2a59eb3f0963f5381a2dde94069e1cc1
A. Maslarov <nsite2014@gmail.com>
2023-01-15 16:12:41 +0200

    Change title after 3sec.

---
+++ src/js/task.js
@@ -0,0 +1,9 @@
+var myFunc = function() {
+    // alert('After 3 seconds of page load!');
+    document.title = "One new message"
+  }
+window.onload = function() {
+    console.log("Hello")
+
+    setTimeout(myFunc, 3000);
+  }
